1eac8f05b8d32b176734655afc41d88fb894d5ee
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-08-29 16:38:38 +0600

    displayed the data form api

--- js/main.js
+++ js/main.js
@@ -17,4 +17,27 @@ const showData= (tools)=>{
 
     const cardContainer=document.getElementById('card-container')
 
+    tools.forEach((tool)=>{
+        console.log(tool.name)
+
+        const div = document.createElement('div')
+
+        div.classList="card w-96 bg-gray-100 shadow-xl"
+
+        div.innerHTML=`
+        <figure><img src="${tool.image}" alt="Tools" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">
+            ${tool.name}
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions">
+          </div>
+        </div>
+        
+        `;
+
+        cardContainer.appendChild(div)
+    })
+
 }
