2d560b96fd5390a8adc2869ac181698f8ed18b7d
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-08-29 21:55:56 +0600

    added dynamic features

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const loadData = async () => {
+const loadData = async (isShowMore) => {
   const res = await fetch("https://openapi.programming-hero.com/api/ai/tools");
 
   const data = await res.json();
@@ -7,30 +7,61 @@ const loadData = async () => {
 
   console.log(tools);
 
-  showData(tools);
+  showData(tools, isShowMore);
 };
 
 loadData();
 
-const showData = (tools) => {
+const showData = (tools, isShowMore) => {
   const cardContainer = document.getElementById("card-container");
 
+  cardContainer.innerHTML = ``;
+
+  if (!isShowMore) {
+    tools = tools.slice(0, 6);
+    handleShowMore();
+  }
+
+  const list = document.getElementById("item-list");
   tools.forEach((tool) => {
     console.log(tool.name);
 
+    const ul = document.createElement("ul");
+    ul.classList.add("list-decimal");
+
+    const features = tool.features;
+
+    features.forEach((feature) => {
+      const li = document.createElement("li");
+      li.innerText = feature;
+      ul.appendChild(li);
+    });
+
     const div = document.createElement("div");
 
-    div.classList = "card w-[30rem] h-[30rem] bg-gray-100 shadow-xl";
+    div.classList =
+      "card max-w-[30rem] max-h-[30rem] bg-base-100  p-4 border-2";
 
     div.innerHTML = `
-        <figure><img src="${tool.image}" alt="Tools" /></figure>
+        <figure><img class="rounded-xl" src="${
+          tool.image ? tool.image : demoPic
+        }" alt="Tools" /></figure>
         <div class="card-body">
-          <h2 class="card-title">
-            ${tool.name}
-          </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions">
+          <div >
+          <p class="text-xl font-bold">Features</p>
+          ${ul.outerHTML}
           </div>
+
+          <hr>
+
+          <h2 class="card-title">
+          ${tool.name}
+        </h2>
+
+            <div class="flex items-center gap-2">
+                <ion-icon name="calendar-outline"></ion-icon>
+                <p> ${tool.published_in}</p>
+            </div>
         </div>
         
         `;
@@ -39,8 +70,17 @@ const showData = (tools) => {
   });
 };
 
-const handleShowMore = () => {
+const handleShowMore = (isTrue) => {
   const showMoreButton = document.getElementById("show-more-btn");
 
-  showMoreButton.classList.remove("hidden");
+  if (!isTrue) {
+    showMoreButton.classList.remove("hidden");
+  } else {
+    showMoreButton.classList.add("hidden");
+  }
+};
+
+const showMore = () => {
+  loadData(true);
+  handleShowMore(true);
 };
